98310a170d841582d6f553c548eb2bc141fb4a01
quinn <quinnjohnson24@gmail.com>
2024-03-17 23:26:57 -0500

    create navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,109 @@
+"use client"
+import React, { ReactNode } from 'react'
+import { Box, VStack, HStack, Link, Image, Text, Flex, Avatar } from "@chakra-ui/react";
+import {
+  BiHome,
+  BiBarChartAlt2,
+  BiCode,
+  BiDetail,
+  BiAperture,
+  BiArrowFromRight
+} from "react-icons/bi";
+import KompletionLogo from "../../../public/kompletionLogo.jpg"
+import { IconButton, Input } from "@chakra-ui/react";
+import { BiSearch } from "react-icons/bi";
+import { signOut } from 'next-auth/react';
+
+//have this take a user. Display a users avatar and jobs
+
+function Navbar({ children }: { children: ReactNode }) {
+  return (
+    <Box className="grid min-h-screen w-full lg:grid-cols-[280px_1fr]">
+      <NavbarVert></NavbarVert>
+      <NavbarHori>{children}</NavbarHori>
+    </Box>
+  )
+}
+
+function NavbarHori({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex flex-col">
+      <header className="flex h-26 lg:h-[80px] items-center gap-4 border-b bg-gray-100/40 px-6 dark:bg-gray-200/40">
+        <Link className="lg:hidden" href="#">
+          <BiCode className="h-6 w-6" />
+        </Link>
+        <div className="w-full flex-1">
+          <Box
+            sx={{
+              p: "8px 4px",
+              display: "flex",
+              alignItems: "center",
+              width: 500,
+            }}
+          >
+            <Input
+              sx={{ ml: 1, flex: 1 }}
+              placeholder="Search Jobs..."
+            />
+            <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+              <BiSearch />
+            </IconButton>
+          </Box>
+        </div>
+        <Avatar></Avatar>
+      </header>
+      {children}
+    </div>
+  )
+}
+
+function NavbarVert() {
+  return (
+    <div className="hidden border-r bg-gray-100/40 lg:block dark:bg-gray-200/40">
+      <div className="flex h-full max-h-screen flex-col gap-2">
+        <div className="flex h-[80px] items-center border-b px-6">
+          <Link
+            className="flex items-center gap-3 rounded-lg px-3 py-2 text-black"
+            href="/home"
+          >
+            <BiDetail></BiDetail>
+            <span>Resume Matcher</span>
+          </Link>
+        </div>
+        <div className="flex-1 overflow-auto py-2">
+          <nav className="grid items-start px-4 text-sm font-medium">
+            <Link
+              className="flex text-lg items-center gap-3 rounded-lg px-3 py-2 text-black transition-all hover:text-gray-400 dark:text-black dark:hover:text-gray-400"
+              href="#"
+            >
+              <BiHome />
+              Home
+            </Link>
+            <Link
+              className="flex text-lg items-center gap-3 rounded-lg px-3 py-2 text-black transition-all hover:text-gray-400 dark:text-black dark:hover:text-gray-400"
+              href="#"
+            >
+              <BiBarChartAlt2 className="h-4 w-4" />
+              Rubric
+            </Link>
+            <Link
+              className="flex text-lg items-center gap-3 rounded-lg px-3 py-2 text-black transition-all hover:text-gray-400 dark:text-black dark:hover:text-gray-400"
+            >
+              <BiAperture className="h-4 w-4" />
+              Settings
+            </Link>
+            <Link
+              className="flex text-lg items-center gap-3 rounded-lg px-3 py-2 text-black transition-all hover:text-gray-400 dark:text-black dark:hover:text-gray-400"
+              onClick={async () => await signOut()}
+              href='/'
+            >
+              <BiArrowFromRight className="h-4 w-4" />
+              Sign Out
+            </Link>
+          </nav>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Navbar
